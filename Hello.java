8311564e421476fc51572e74a644af1232e049e4
Ilasinghal <ilasinghal.1995@gmail.com>
2023-10-31 16:09:43 +0530

    file uploaded

---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello{  
+    public static void main(String[] args){  
+    System.out.println("This is java app "); 
+    System.out.println("This is java app by using Jenkins");
+    System.out.println("This is java app 2nd build");  
+    System.out.println("changes done reflect on jenkins dashboard");
+      
+    }  
+    }  
